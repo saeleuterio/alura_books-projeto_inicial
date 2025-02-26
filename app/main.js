5c7c03a66bee777b9bf1ea9d09b7b65341057986
saeleuterio <saeleuterio@hotmail.com>
2025-02-26 17:49:14 -0300

    up

--- app/main.js
+++ app/main.js
@@ -5,5 +5,6 @@ getBuscarLivrosDaAPI()
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
-    console.table(livros)
+    let livrosComDesconto = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
