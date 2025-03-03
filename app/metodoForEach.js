0dd9fed98d3367bfc1ea8011bbdce8a46040dcb6
saeleuterio <saeleuterio@hotmail.com>
2025-03-03 11:13:39 -0300

    up

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,10 +1,12 @@
 const elementoParaInserirLivros = document.getElementById('livros')
 
 function exibirOsLivrosNaTela(listaDeLivros) {
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel'
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}"
+            <img class="${disponibilidade}" src="${livro.imagem}"
             alt="${livro.alt}" />
             <h2 class="livro__titulo">${livro.titulo}</h2>
             <p class="livro__descricao">${livro.autor}</p>
